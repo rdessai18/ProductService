be5627104fda3dcf09b79567eb0cf1e16ef5dd2e
rdessai18 <dessairajat56@gmail.com>
2024-03-15 17:46:47 +0530

    Basuc Security Configuration

---
+++ src/main/java/com/scaler/project/productservice/security/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.scaler.project.productservice.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(request -> request
+                        .anyRequest()
+                        .authenticated())
+                .build();
+    }
+}
